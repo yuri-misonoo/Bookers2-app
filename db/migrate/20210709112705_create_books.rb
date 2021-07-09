82cc7597c04567c4c96d9047cc687b19cce2f847
yuri-misonoo <yuri.panpanda@gmail.com>
2021-07-09 12:50:00 +0000

    [add]ファイルの準備

---
+++ db/migrate/20210709112705_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.integer :book_id
+      t.text :title
+      t.text :body
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
