7acc8eecbbb000a1048d1f2d225ac83c29ab8b2a
yuri-misonoo <yuri.panpanda@gmail.com>
2021-07-16 05:35:15 +0000

    [Fix]userのcreateアクション削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,6 @@
 class UsersController < ApplicationController
   before_action :ensure_correct_user,only: [:edit, :update]
 
-  def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
-      flash[:notice] = "You have created book successfully."
-      redirect_to book_path(@book.id)
-    else
-      render template: 'books/index'
-    end
-  end
-
   def index
     @book = Book.new
     @users = User.all
