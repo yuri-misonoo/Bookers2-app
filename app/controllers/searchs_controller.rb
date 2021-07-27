5506f91a2ef1d00aed57cbcf928dc78ca955081b
yuri-misonoo <yuri.panpanda@gmail.com>
2021-07-27 07:46:21 +0000

    ※[Fix]応用課題５

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -13,12 +13,21 @@ class SearchsController < ApplicationController
     if model == 'user'
       if method == 'perfect'
         User.where(name: content)
+      elsif method == 'forward'
+        User.where('name LIKE ?', content+'%')
+      elsif method == 'backward'
+        User.where('name LIKE ?', '%'+content)
       else
         User.where('name LIKE ?', '%'+content+'%')
       end
+      
     elsif model == 'book'
       if method == 'perfect'
-        Book.where('title: content')
+        Book.where(title: content)
+      elsif method == 'forward'
+        Book.where('title LIKE ?', content+'%')
+      elsif method == 'backward'
+        Book.where('title LIKE ?', '%'+content)
       else
         Book.where('title LIKE ?', '%'+content+'%')
       end
